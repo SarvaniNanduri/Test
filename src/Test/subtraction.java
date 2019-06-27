053e57b5225a4ef9349874b124e6bc72b3d5bf05
sarvani nanduri <sarvani.nanduri@aspiresys.com>
2019-06-27 12:27:37 +0530

    Testing automatic build of multiple files

---
+++ src/Test/subtraction.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class subtraction {
+	public static void main(String args[])
+	{
+		int a=90,b=70,c;
+		c = a - b;
+		System.out.println("Subtraction of two number a and b is  : "+ c);
+	}
+}
