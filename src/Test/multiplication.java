9171cf52028e48eec51ca8d75ba70792a027823d
sarvani nanduri <sarvani.nanduri@aspiresys.com>
2019-06-27 12:29:15 +0530

    Added multiplication to subraction file to check multiple files build

---
+++ src/Test/multiplication.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class multiplication {
+	public static void main(String args[])
+	{
+		int a=5,b=7,c;
+		c = a * b;
+		System.out.println("Multiplication of two number a and b is  : "+ c);
+	}
+}
