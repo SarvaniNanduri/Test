9537304dcbfd00d1c67714ad9f033c78a032ea99
sarvani nanduri <sarvani.nanduri@aspiresys.com>
2019-06-27 11:10:56 +0530

    Testing build by creating repo from eclipse'
    :
    
    addition.

---
+++ src/Test/addition.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class addition {
+	
+	public static void main(String args[])
+	{
+		int a=10,b=7,c;
+		c = a + b;
+		System.out.println("Addition of two number a and b is  : "+ c);
+	}
+	
+
+}
